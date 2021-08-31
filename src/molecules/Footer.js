f0ba0ef45e1927e50d89d07f5d37597577486ac4
truxxu <danielfetrujillo@gmail.com>
2021-08-31 16:07:02 -0500

    added header

--- src/molecules/Footer.js
+++ src/molecules/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import lightning from "../images/lightning.svg";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div class="container">
+      <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
+        <span class="text-muted">&spades; 2021 LN Lotto</span>
+      </footer>
+    </div>
+  );
 };
 
 export { Footer };
