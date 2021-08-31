51c92b16b9b61ed61672a8d5179240cd590dce3f
truxxu <danielfetrujillo@gmail.com>
2021-08-31 15:21:47 -0500

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.module.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import styles from "./App.module.scss";
 
 function App() {
-  return <diV>Hello World!</diV>;
+  return (
+    <div className="container-fluid">
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/tickets">
+              <p>Tickets purchase page</p>
+            </Route>
+            <Route path="/">
+              <p>Welcome page</p>
+            </Route>
+          </Switch>
+        </Router>
+      </main>
+    </div>
+  );
 }
 
 export default App;
